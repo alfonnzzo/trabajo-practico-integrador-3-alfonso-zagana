a17ce6b9941725158677e89f236517c1444fedaf
allfonzzo <a.zaganatorancio@gmail.com>
2025-11-27 22:39:58 -0300

    Comprobando que el login funcione

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,45 @@
+import { Link } from "react-router";
+import { useForm } from "../hooks/useForm";
+
 export const LoginPage = () => {
+  const { formState, handleChange } = useForm({
+    username: "",
+    password: "",
+  });
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("Formulario funcionando:", formState);
+    alert("Funciona 👍");
+  };
+
   return (
-    <form>
-      <label>Username</label>
-    </form>
+    <main>
+      <h1>Login</h1>
+
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Usuario</label>
+          <input
+            type="text"
+            name="username"
+            value={formState.username}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label>Contraseña</label>
+          <input
+            type="password"
+            name="password"
+            value={formState.password}
+            onChange={handleChange}
+          />
+        </div>
+
+        <button type="submit">Ingresar</button>
+      </form>
+    </main>
   );
 };
